ad7de1231f6ebf6f845c6a19e3f139963e76dcf1
Thomas DAVID <thomas.david@mines.org>
2016-10-16 20:42:29 +0000

    TRC-STS is executing (with a lot of hard code ...)

--- tomcat/create_host_env.sh
+++ tomcat/create_host_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DL_DIR=/opt/openncp-downloads
+CACHE_DIR=/opt/openncp-cache
 COUNTRY_CODE=fr
 PASSWORD=rootroot
 ORGANIZATION=sante.gouv.fr
@@ -20,17 +21,39 @@ fi
 
 if [ -d $DL_DIR ]
 then
-  echo "^DL_DIR present. Exiting."
+  echo "$DL_DIR present. Exiting."
   exit
 fi
 
 mkdir  $DL_DIR
 
+if [ -d $CACHE_DIR ]
+then
+  echo "$CACHE_DIR present. Exiting."
+  exit
+fi
+
+mkdir  $CACHE_DIR
+
+
 wget https://openncp.atlassian.net/wiki/download/attachments/65142795/epsos-configuration.zip?api=v2 -O $DL_DIR/epsos-configuration.zip
 unzip $DL_DIR/epsos-configuration.zip  -d /opt
 
 
+
 mkdir -p /opt/epsos-configuration/cert/PPT/conf
+
+
+# Properties init
+
+wget https://openncp.atlassian.net/wiki/download/attachments/65142795/OpenNCP-configuration-utility.zip?api=v2 -O $DL_DIR/OpenNCP-configuration-utility.zip
+unzip $DL_DIR/OpenNCP-configuration-utility.zip -d /opt/epsos-configuration/
+
+
+sed -i "s/>username/>$DATABASE_LOGIN/" /opt/epsos-configuration/OpenNCP-configuration-utility/database.config.xml
+sed -i "s/>password/>$PASSWORD/" /opt/epsos-configuration/OpenNCP-configuration-utility/database.config.xml
+sed -i "s/databasehost/$DATABASE/" /opt/epsos-configuration/OpenNCP-configuration-utility/database.config.xml
+sed -i "s/databasename/epsos_properties/" /opt/epsos-configuration/OpenNCP-configuration-utility/database.config.xml
 
 #cacert.sh
 
@@ -83,6 +106,32 @@ sed -i "s/mysql:\/\/databasehost/mysql:\/\/$DATABASE/" /opt/epsos-configuration/
 sed -i "s/>username/>$DATABASE_LOGIN/" /opt/epsos-configuration/configmanager.hibernate.xml
 sed -i "s/>password/>$PASSWORD/" /opt/epsos-configuration/configmanager.hibernate.xml
 
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE hibernate-configuration PUBLIC "-//Hibernate/Hibernate Configuration DTD 3.0//EN" "http://hibernate.sourceforge.net/hibernate-configuration-3.0.dtd">
+<hibernate-configuration>
+    <session-factory>
+        <property name="hibernate.dialect">org.hibernate.dialect.MySQLDialect</property>
+        <property name="hibernate.connection.driver_class">com.mysql.jdbc.Driver</property>
+        <property name="hibernate.connection.url">jdbc:mysql://mysql:3306/epsos_properties?zeroDateTimeBehavior=convertToNull</property>
+        <property name="hibernate.connection.username">root</property>
+        <property name="hibernate.connection.password">mypassword</property>
+        <!-- JDBC connection pool (use the built-in) -->
+        <property name="connection.pool_size">1</property>
+        <property name="current_session_context_class">thread</property>
+        <!-- Disable the second-level cache -->
+        <property name="cache.provider_class">org.hibernate.cache.NoCacheProvider</property>
+        <!-- Echo all executed SQL to stdout -->
+        <property name="show_sql">yes</property>
+        <property name="hibernate.hbm2ddl.auto">update</property>
+        <mapping class="eu.epsos.configmanager.database.model.Property"/>
+    </session-factory>
+</hibernate-configuration>
+' >  /opt/epsos-configuration/database.config.xml
+
+
+
+# TRC-STS
+wget https://joinup.ec.europa.eu/nexus/content/repositories/releases/eu/europa/ec/joinup/ecc/epsos-trc-sts/2.3.2/epsos-trc-sts-2.3.2.war -O $CACHE_DIR/TRC-STS.war
 
 rm -fr $DL_DIR
 exit
